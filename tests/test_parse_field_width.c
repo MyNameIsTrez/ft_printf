0e41862b72aea6e02382debe9a08adb621f30493
MyNameIsTrez <welfje@gmail.com>
2022-01-28 15:01:20 +0100

    Add tests for parse_field_width()

--- tests/test_parse_field_width.c
+++ tests/test_parse_field_width.c
@@ -1,33 +1,52 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        ::::::::            */
-// /*   test_parse_field_width.c                           :+:    :+:            */
-// /*                                                     +:+                    */
-// /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
-// /*                                                   +#+                      */
-// /*   Created: 2022/01/20 11:34:27 by sbos          #+#    #+#                 */
-// /*   Updated: 2022/01/26 10:55:05 by sbos          ########   odam.nl         */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   test_parse_field_width.c                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/20 11:34:27 by sbos          #+#    #+#                 */
+/*   Updated: 2022/01/28 13:25:46 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
 
-// ////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-// #include "tests.h"
+#include "tests.h"
 
-// ////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-// void	test_parse_field_width(void)
-// {
-// 	const char *format = "42d";
-// 	t_options	options;
+void	test_parse_field_width(void)
+{
+	{
+		const char *format = "42d";
+		t_options	options;
 
-// 	parse_field_width(&format, &options);
+		initialize_options(&options);
+		parse_field_width(&format, &options);
 
-// 	bool	tested_value;
-// 	bool	expected_value = 42;
+		ASSERT_INT(options.field_width, 42);
+	}
 
-// 	tested_value = options.field_width;
-// 	assert(tested_value == expected_value);
-// }
+	{
+		const char *format = "d";
+		t_options	options;
 
-// ////////////////////////////////////////////////////////////////////////////////
+		initialize_options(&options);
+		parse_field_width(&format, &options);
+
+		ASSERT_INT(options.field_width, 0);
+	}
+
+	{
+		const char *format = "0d";
+		t_options	options;
+
+		initialize_options(&options);
+		parse_field_width(&format, &options);
+
+		ASSERT_INT(options.field_width, 0);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
