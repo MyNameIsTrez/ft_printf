99104270575e9e062f52c07585fa8186a274e77a
MyNameIsTrez <welfje@gmail.com>
2022-03-31 16:25:28 +0200

    Replace libft symlink with copy.

---
+++ libft/src/nbr/ft_atoi.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_atoi.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/11 12:41:20 by sbos          #+#    #+#                 */
+/*   Updated: 2022/03/29 14:46:43 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "libft.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+static long	get_number_without_minus(const char *str)
+{
+	long	integer;
+	size_t	i;
+	char	chr;
+
+	integer = 0;
+	i = 0;
+	while (ft_isdigit(str[i]))
+	{
+		chr = str[i];
+		if (ft_char_to_digit(chr) == -1)
+			return (0);
+		integer *= 10;
+		integer += ft_char_to_digit(chr);
+		i++;
+	}
+	return (integer);
+}
+
+/**
+ * @brief Converts @p str to a signed int.
+ *
+ * @param str May start with whitespace, and the first encountered number will
+ * be converted to a signed int.
+ * @return The converted value or 0 if no number was found in @p str.
+ */
+int	ft_atoi(const char *str)
+{
+	long	sign;
+	int		result;
+
+	while (ft_isspace(str[0]))
+		str++;
+	sign = 1;
+	if (str[0] == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (str[0] == '+')
+		str++;
+	result = (int)(sign * get_number_without_minus(str));
+	return (result);
+}
+
+////////////////////////////////////////////////////////////////////////////////
